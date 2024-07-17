8bebadd8da63fec41113ce3661c5c115f9637d03
wanny <choijw1116@gmail.com>
2024-07-17 20:58:19 +0900

    [Fix]Fix ReadExchangePostRes DTO

--- src/main/java/com/example/mojal2ndproject2/exchangepost/model/dto/response/ReadExchangePostRes.java
+++ src/main/java/com/example/mojal2ndproject2/exchangepost/model/dto/response/ReadExchangePostRes.java
@@ -1,13 +1,25 @@
 package com.example.mojal2ndproject2.exchangepost.model.dto.response;
 
+import com.example.mojal2ndproject2.category.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class ReadExchangePostRes {
     private Long idx;
+    private Long memberIdx;
+    private String giveBtmCategory;
+    private String takeBtmCategory;
+    private String giveCategory;
+    private String takeCategory;
     private String title;
     private String contents;
     private String timeStamp;
     private String modifyTime;
     private Boolean status;
     private Boolean postType;
-    private String giveBtmCategory;
-    private String takeBtmCategory;
+
 }
