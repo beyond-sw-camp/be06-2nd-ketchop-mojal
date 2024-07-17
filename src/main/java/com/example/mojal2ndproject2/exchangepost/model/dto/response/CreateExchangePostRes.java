a30d1b2d00f95266ae7bd321ee03e3a8e9c8c8c7
wanny <choijw1116@gmail.com>
2024-07-17 17:39:03 +0900

    [Feat] Add CreateExchangePostRes DTO

--- src/main/java/com/example/mojal2ndproject2/exchangepost/model/dto/response/CreateExchangePostRes.java
+++ src/main/java/com/example/mojal2ndproject2/exchangepost/model/dto/response/CreateExchangePostRes.java
@@ -1,37 +1,15 @@
 package com.example.mojal2ndproject2.exchangepost.model.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class CreateExchangePostRes {
-    private String category;
+    private Long idx;
+    private String name;
     private String title;
     private String content;
-
-    public CreateExchangePostRes(String category, String title, String content) {
-        this.category = category;
-        this.title = title;
-        this.content = content;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
