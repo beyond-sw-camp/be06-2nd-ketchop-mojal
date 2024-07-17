a1369d6208275eeddddd4b7733c97a8d21abf63e
wanny <choijw1116@gmail.com>
2024-07-17 17:39:51 +0900

    [Feat]Add ExchangePostController

--- src/main/java/com/example/mojal2ndproject2/exchangepost/ExchangePostController.java
+++ src/main/java/com/example/mojal2ndproject2/exchangepost/ExchangePostController.java
@@ -1,16 +1,17 @@
 package com.example.mojal2ndproject2.exchangepost;
 
-import com.example.mojal2ndproject2.exchangepost.model.ExchangePost;
 import com.example.mojal2ndproject2.exchangepost.model.dto.request.CreateExchangePostReq;
 import com.example.mojal2ndproject2.exchangepost.model.dto.response.CreateExchangePostRes;
 import com.example.mojal2ndproject2.exchangepost.model.dto.response.ReadExchangePostRes;
+import com.example.mojal2ndproject2.member.model.CustomUserDetails;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/exchangepost")
+@RequestMapping("/post/exchange")
 public class ExchangePostController {
     private ExchangePostService exchangePostService;
 
@@ -19,25 +20,25 @@ public class ExchangePostController {
     }
 
     //교환글생성
-    @RequestMapping(method = RequestMethod.POST, name = "/create")
-    public ResponseEntity<CreateExchangePostRes> create(@RequestBody CreateExchangePostReq req) {
-        CreateExchangePostRes res = exchangePostService.create(req);
+    @RequestMapping(method = RequestMethod.POST, value = "/users/create")
+    public ResponseEntity<CreateExchangePostRes> create( @AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody CreateExchangePostReq req) {
+        CreateExchangePostRes res = exchangePostService.create(req, customUserDetails);
         return ResponseEntity.ok(res);
     }
 
-    //교환글전체조회
-    @RequestMapping(method = RequestMethod.GET, name = "/list")
-    public ResponseEntity<List<ReadExchangePostRes>> list() {
-        List<ReadExchangePostRes> res = exchangePostService.list();
-        return ResponseEntity.ok(res);
-    }
-
-
-    //교환글해당글조회
-    @RequestMapping(method = RequestMethod.GET, name = "/read")
-    public ResponseEntity<ReadExchangePostRes> read(@RequestParam Long id) {
-        ReadExchangePostRes res = exchangePostService.read(id);
-        return ResponseEntity.ok(res);
-    }
+//    //교환글전체조회
+//    @RequestMapping(method = RequestMethod.GET, name = "/list")
+//    public ResponseEntity<List<ReadExchangePostRes>> list() {
+//        List<ReadExchangePostRes> res = exchangePostService.list();
+//        return ResponseEntity.ok(res);
+//    }
+//
+//
+//    //교환글해당글조회
+//    @RequestMapping(method = RequestMethod.GET, name = "/read")
+//    public ResponseEntity<ReadExchangePostRes> read(@RequestParam Long id) {
+//        ReadExchangePostRes res = exchangePostService.read(id);
+//        return ResponseEntity.ok(res);
+//    }
 
 }
