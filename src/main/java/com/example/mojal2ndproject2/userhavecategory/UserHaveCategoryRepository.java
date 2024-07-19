74979c22f57b3477f0e4a611f36940f5ba6aeef8
eunjooNine <eunjoo.nine@gmail.com>
2024-07-19 10:25:41 +0900

    [feat] #14 matching feat

--- src/main/java/com/example/mojal2ndproject2/userhavecategory/UserHaveCategoryRepository.java
+++ src/main/java/com/example/mojal2ndproject2/userhavecategory/UserHaveCategoryRepository.java
@@ -1,7 +1,13 @@
 package com.example.mojal2ndproject2.userhavecategory;
 
+import com.example.mojal2ndproject2.category.Category;
+import com.example.mojal2ndproject2.member.model.Member;
 import com.example.mojal2ndproject2.userhavecategory.model.UserHaveCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserHaveCategoryRepository extends JpaRepository<UserHaveCategory, Long> {
+    Optional<UserHaveCategory> findByMemberAndCategory(Member member, Category category);
 }
