8f09ec99789cf4afff66caed621c0ea02e2e200c
wanny <choijw1116@gmail.com>
2024-07-19 14:29:46 +0900

    [Feat] Add Status(회원가입시 카테고리설정안했을때 예외처리-sharepost,exchangepost)

--- src/main/java/com/example/mojal2ndproject2/common/BaseResponseStatus.java
+++ src/main/java/com/example/mojal2ndproject2/common/BaseResponseStatus.java
@@ -26,6 +26,9 @@ public enum BaseResponseStatus {
     /**2200 : Login에러**/
     MEMBER_NOT_LOGIN(false,2204,"로그인해주세요."),
 
+    /**2100 : Login에러**/
+    CHECK_CATEGORY_MORE_THAN_ONE(false,2101,"내 재능을 하나 이상 체크 해 주세요."),
+
     /**
      * 3000 : MEMBER 에러
      */
