677e4641cd14e8513aed952bbc613f858c011d6e
shinebyul <qufdl8382@gmail.com>
2024-07-19 11:12:19 +0900

    [Valid]회원관련 request dto validation처리

--- src/main/java/com/example/mojal2ndproject2/member/model/dto/request/MemberLoginReq.java
+++ src/main/java/com/example/mojal2ndproject2/member/model/dto/request/MemberLoginReq.java
@@ -1,5 +1,8 @@
 package com.example.mojal2ndproject2.member.model.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +13,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberLoginReq {
+    @Email(message = "이메일 형식을 확인해주세요.")
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @NotNull(message = "이메일을 입력해주세요.")
     private String email;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @NotNull(message = "비밀번호를 입력해주세요.")
     private String password;
 }
