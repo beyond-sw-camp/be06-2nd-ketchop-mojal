559c79d0591f23ad482e592a4023c9c58bf35681
shinebyul <qufdl8382@gmail.com>
2024-07-21 18:07:41 +0900

    dataInit

--- src/main/java/com/example/mojal2ndproject2/category/DataInit.java
+++ src/main/java/com/example/mojal2ndproject2/category/DataInit.java
@@ -2,9 +2,14 @@ package com.example.mojal2ndproject2.category;
 
 import com.example.mojal2ndproject2.exchangepost.ExchangePostRepository;
 import com.example.mojal2ndproject2.exchangepost.model.ExchangePost;
+import com.example.mojal2ndproject2.matching.PostMatchingMemberRepository;
+import com.example.mojal2ndproject2.matching.model.PostMatchingMember;
+import com.example.mojal2ndproject2.member.MemberRepository;
 import com.example.mojal2ndproject2.member.model.Member;
 import com.example.mojal2ndproject2.sharePost.SharePostRepository;
 import com.example.mojal2ndproject2.sharePost.model.SharePost;
+import com.example.mojal2ndproject2.userhavecategory.UserHaveCategoryRepository;
+import com.example.mojal2ndproject2.userhavecategory.model.UserHaveCategory;
 import jakarta.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.util.Set;
@@ -18,8 +23,11 @@ public class DataInit {
     private final CategoryRepository categoryRepository;
     private final SharePostRepository sharePostRepository;
     private final ExchangePostRepository exchangePostRepository;
+    private final MemberRepository memberRepository;
+    private final UserHaveCategoryRepository userHaveCategoryRepository;
+    private final PostMatchingMemberRepository postMatchingMemberRepository;
 
-//    @PostConstruct
+    @PostConstruct
     public void dataInsert() {
 
         Set<String> categories = Set.of(
@@ -35,14 +43,55 @@ public class DataInit {
             categoryRepository.save(category);
         }
 
-        //나눔글 생성
+        //member 1,2,3,4 생성
+        for(int i=1;i<=4;i++){
+            if(i==3){
+                Member member = Member.builder()
+                        .email("member"+i+"@email.com")
+                        .nickname("member"+i)
+                        .password("$2a$10$Rsya1PvPOGeDGcLThcgji.oP9RTgf8zzvHF5TwnHXIGX1hqvDC/QC")
+                        .role("ROLE_USER")
+                        .emailAuth(true)
+                        .signupDate(LocalDateTime.now())
+                        .build();
+                Member saved = memberRepository.save(member);
+                Set<Long> userCategories = Set.of(1L, 3L, 14L);
+                for (Long userCategory : userCategories) {
+                    UserHaveCategory userHaveCategory = UserHaveCategory.builder()
+                            .category(Category.builder().idx(userCategory).build())
+                            .member(saved)
+                            .build();
+                    userHaveCategoryRepository.save(userHaveCategory);
+                }
+            }else{
+                Member member = Member.builder()
+                        .email("member"+i+"@email.com")
+                        .nickname("member"+i)
+                        .password("$2a$10$Rsya1PvPOGeDGcLThcgji.oP9RTgf8zzvHF5TwnHXIGX1hqvDC/QC")
+                        .role("ROLE_USER")
+                        .emailAuth(true)
+                        .signupDate(LocalDateTime.now())
+                        .build();
+                Member saved = memberRepository.save(member);
+                Set<Long> userCategories = Set.of(1L, 2L, 14L);
+                for (Long userCategory : userCategories) {
+                    UserHaveCategory userHaveCategory = UserHaveCategory.builder()
+                            .category(Category.builder().idx(userCategory).build())
+                            .member(saved)
+                            .build();
+                    userHaveCategoryRepository.save(userHaveCategory);
+                }
+            }
+        }
+
+        //member1 나눔글 생성
         for(int i=1;i<=30;i++){
             SharePost sharePost = SharePost.builder()
                     .member(Member.builder().idx(1L).build())
                     .btmCategory("bottom catrgory")
-                    .title("title"+i)
+                    .title("member1 title"+i)
                     .contents("content"+i)
-                    .category(Category.builder().idx(1L).build())
+                    .category(Category.builder().idx(14L).build())
                     .currentEnrollment(0)
                     .capacity(5)
                     .deadline("10")
@@ -53,14 +102,14 @@ public class DataInit {
                     .build();
             sharePostRepository.save(sharePost);
         }
-
-        for(int i=31;i<60;i++){
+        //member2 나눔글 생성
+        for(int i=31;i<32;i++){
             SharePost sharePost = SharePost.builder()
                     .member(Member.builder().idx(2L).build())
                     .btmCategory("bottom catrgory")
-                    .title("title"+i)
+                    .title("member2 title"+i)
                     .contents("content"+i)
-                    .category(Category.builder().idx(1L).build())
+                    .category(Category.builder().idx(14L).build())
                     .currentEnrollment(0)
                     .capacity(5)
                     .deadline("10")
@@ -72,8 +121,57 @@ public class DataInit {
             sharePostRepository.save(sharePost);
         }
 
+        //member3 나눔글 생성
+        for(int i=32;i<33;i++){
+            SharePost sharePost = SharePost.builder()
+                    .member(Member.builder().idx(3L).build())
+                    .btmCategory("java")
+                    .title("member3 title"+i)
+                    .contents("content"+i)
+                    .category(Category.builder().idx(14L).build())
+                    .currentEnrollment(1)
+                    .capacity(1)
+                    .deadline("10")
+                    .postType("share")
+                    .status(true)
+                    .timeStamp(LocalDateTime.now())
+                    .modifyTime(LocalDateTime.now())
+                    .build();
+            SharePost saved = sharePostRepository.save(sharePost);
+            //member2가 3의 글에 참여
+            PostMatchingMember postMatchingMember = PostMatchingMember.builder()
+                    .sharePost(saved)
+                    .member(Member.builder().idx(2L).build())
+                    .build();
+            postMatchingMemberRepository.save(postMatchingMember);
+        }
 
-        //교환글 생성
+
+        //member4 나눔글 생성
+        for(int i=33;i<34;i++){
+            SharePost sharePost = SharePost.builder()
+                    .member(Member.builder().idx(4L).build())
+                    .btmCategory("java")
+                    .title("member3 title"+i)
+                    .contents("content"+i)
+                    .category(Category.builder().idx(14L).build())
+                    .currentEnrollment(1)
+                    .capacity(5)
+                    .deadline("10")
+                    .postType("share")
+                    .status(true)
+                    .timeStamp(LocalDateTime.now())
+                    .modifyTime(LocalDateTime.now())
+                    .build();
+            SharePost saved = sharePostRepository.save(sharePost);
+            //member1이 4에 참여
+            PostMatchingMember postMatchingMember2 = PostMatchingMember.builder()
+                    .sharePost(saved)
+                    .member(Member.builder().idx(1L).build()).build();
+            postMatchingMemberRepository.save(postMatchingMember2);
+        }
+
+        //member1 교환글 생성
         for(int i=1;i<=30;i++){
             ExchangePost exchangePost = ExchangePost.builder()
                     .member(Member.builder().idx(1L).build())
@@ -86,12 +184,13 @@ public class DataInit {
                     .giveCategory(Category.builder().idx(1L).build())
                     .title("title"+i)
                     .takeBtmCategory("give btm category")
-                    .takeCategory(Category.builder().idx(1L).build())
+                    .takeCategory(Category.builder().idx(2L).build())
                     .build();
             exchangePostRepository.save(exchangePost);
         }
 
-        for(int i=1;i<=30;i++){
+        //member2 교환글 생성
+        for(int i=31;i<32;i++){
             ExchangePost exchangePost = ExchangePost.builder()
                     .member(Member.builder().idx(2L).build())
                     .contents("content"+i)
@@ -103,7 +202,25 @@ public class DataInit {
                     .giveCategory(Category.builder().idx(1L).build())
                     .title("title"+i)
                     .takeBtmCategory("give btm category")
-                    .takeCategory(Category.builder().idx(1L).build())
+                    .takeCategory(Category.builder().idx(2L).build())
+                    .build();
+            exchangePostRepository.save(exchangePost);
+        }
+
+        //member3 교환글 생성
+        for(int i=32;i<33;i++){
+            ExchangePost exchangePost = ExchangePost.builder()
+                    .member(Member.builder().idx(3L).build())
+                    .contents("content"+i)
+                    .postType("exchange")
+                    .status(false)
+                    .modifyTime(LocalDateTime.now())
+                    .timeStamp(LocalDateTime.now())
+                    .giveBtmCategory("give btm category")
+                    .giveCategory(Category.builder().idx(1L).build())
+                    .title("title"+i)
+                    .takeBtmCategory("give btm category")
+                    .takeCategory(Category.builder().idx(5L).build())
                     .build();
             exchangePostRepository.save(exchangePost);
         }
